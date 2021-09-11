e5714438d4898c5fd307b1274669423ae532fdaf
Kawarabata <kwrbt0404@gmail.com>
2021-09-11 16:18:52 +0900

    Initial commit

---
+++ lib/css_variables_snippets.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "css_variables_snippets/version"
+
+module CssVariablesSnippets
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/css_variables_snippets/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CssVariablesSnippets
+  VERSION = "0.1.0"
+end
